4fdeb4ebf5bb39110a09bf4e77fdc4105e4d8ed1
dark-noob830 <mahdiamr83@gmail.com>
2026-06-01 11:19:15 +0330

    init: project

---
+++ A/A/config/security.py
@@ -0,0 +1,45 @@
+from .base import *
+
+if DEBUG:
+
+    CORS_ALLOW_ALL_ORIGINS = True
+    ALLOWED_HOSTS = ["*"]
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+    SECURE_HSTS_SECONDS = 0
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+    SECURE_HSTS_PRELOAD = False
+else:
+    CORS_ALLOWED_ORIGINS = [
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+    ]
+    ALLOWED_HOSTS = ['domain', 'localhost', '127.0.0.1']
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    CSRF_TRUSTED_ORIGINS = ['https://domain']
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+CORS_ALLOW_CREDENTIALS = False
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
---
+++ A/A/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+]
+
+if 'debug_toolbar' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
