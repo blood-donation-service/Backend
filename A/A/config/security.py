07c41b8f3f6173b4d2edf981357c00d8548e7e62
dark-noob830 <mahdiamr83@gmail.com>
2026-07-06 04:30:44 +0330

    feat: for deploy add nginx

--- A/A/config/security.py
+++ A/A/config/security.py
@@ -15,7 +15,12 @@ else:
         'http://127.0.0.1:5173',
         'http://localhost:5173',
     ]
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+        '.sslip.io',
+        '.dokploy.com',
+    ]
 
     INTERNAL_IPS = [
         "127.0.0.1",
